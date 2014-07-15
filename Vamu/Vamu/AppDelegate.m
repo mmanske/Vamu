89b7ed891d56592011fa159cac8afbecfa9252db
mmanske <desenv001@macbook-001.bematech.com>
2014-07-15 11:51:21 -0300

    Register for push notification

--- Vamu/Vamu/AppDelegate.m
+++ Vamu/Vamu/AppDelegate.m
@@ -33,9 +33,21 @@
     
     [GMSServices provideAPIKey:@"AIzaSyAmjpaFgO4GisCiSBkP3paAE2yURhmCwMc"];
     
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    
     return YES;
 }
-							
+
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+	NSLog(@"My token is: %@", deviceToken);
+}
+
+
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
